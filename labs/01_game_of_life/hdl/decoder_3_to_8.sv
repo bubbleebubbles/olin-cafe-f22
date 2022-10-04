ff05e336e1dc6248c21d2360c5848710e342a19c
bubbleebubbles <dasha@chadyuk.com>
2022-10-04 09:27:56 -0400

    completed decoder_3_to_8.sv

--- labs/01_game_of_life/hdl/decoder_3_to_8.sv
+++ labs/01_game_of_life/hdl/decoder_3_to_8.sv
@@ -5,5 +5,17 @@ module decoder_3_to_8(ena, in, out);
   input wire [2:0] in;
   output logic [7:0] out;
 
+    
+    assign out[0] = ena&~in[0]&~in[1]&~in[2];
+    assign out[1] = ena&in[0]&~in[1]&~in[2];
+    assign out[2] = ena&~in[0]&in[1]&~in[2];
+    assign out[3] = ena&in[0]&in[1]&~in[2]; 
+
+    assign out[4] = ena&~in[0]&~in[1]&in[2]; 
+    assign out[5] = ena&in[0]&~in[1]&in[2];
+    assign out[6] = ena&~in[0]&in[1]&in[2];
+    assign out[7] = ena&in[0]&in[1]&in[2]; 
+
+
 
 endmodule

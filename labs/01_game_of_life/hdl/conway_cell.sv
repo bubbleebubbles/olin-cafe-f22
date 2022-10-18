129d54eefe4908e8dbf1374b60e57077d2cc1930
Cara Mulrooney <cmulrooney@olin.edu>
2022-10-18 17:24:16 -0400

    Edit conway_cell comments

--- labs/01_game_of_life/hdl/conway_cell.sv
+++ labs/01_game_of_life/hdl/conway_cell.sv
@@ -9,11 +9,12 @@ input wire ena;
 input wire state_0;
 output logic state_d; // NOTE - this is only an output of the module for debugging purposes. 
 output logic state_q;
+logic next_state; 
 
-logic live_neighbors_3;
-logic continue_alive;
-logic enabled_output;
-logic reset_output;
+//logic live_neighbors_3;
+//logic continue_alive;
+//logic enabled_output;
+//logic reset_output;
 
 input wire [7:0] neighbors;
 logic [3:0] live_neighbors;
@@ -34,10 +35,9 @@ adder_1 SUM_N78(.a(neighbors[6]),.b(neighbors[7]),.c_in(1'b0),.sum(n78[0]),.c_ou
 adder_parallel_2 SUM_N1234(.a(n12),.b(n34),.c_in(1'b0),.sum(n1234[1:0]),.c_out(n1234[2]));
 adder_parallel_2 SUM_N5678(.a(n56),.b(n78),.c_in(1'b0),.sum(n5678[1:0]),.c_out(n5678[2]));
 
-// Sum neighbors using a 3-bit parallel adder
+// Sum neighbors using a 3-bit parallel adder to find the total number of living neighbors
 adder_parallel_3 SUM_N(.a(n1234),.b(n5678),.c_in(1'b0),.sum(live_neighbors[2:0]),.c_out(live_neighbors[3]));
 
-logic next_state; 
 always_comb begin: INPUT_LOGIC
     next_state = (~live_neighbors[3]&~live_neighbors[2]&live_neighbors[1])& ((state_q & ~live_neighbors[0])|live_neighbors[0]); 
     state_d = ~rst ? (ena ? next_state : state_q) : state_0;

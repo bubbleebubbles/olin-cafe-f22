6b1baaccd948447b70085f7b205b179858daccc4
bubbleebubbles <dasha@chadyuk.com>
2022-10-18 20:04:53 -0400

    added comments to conway_cell and led_array_driver

--- labs/01_game_of_life/hdl/conway_cell.sv
+++ labs/01_game_of_life/hdl/conway_cell.sv
@@ -10,10 +10,7 @@ input wire state_0;
 output logic state_d; // NOTE - this is only an output of the module for debugging purposes. 
 output logic state_q;
 
-logic live_neighbors_3;
-logic continue_alive;
-logic enabled_output;
-logic reset_output;
+logic next_state; //Used when computing conway logic for the cell's next state 
 
 input wire [7:0] neighbors;
 logic [3:0] live_neighbors;
@@ -37,22 +34,10 @@ adder_parallel_2 SUM_N5678(.a(n56),.b(n78),.c_in(1'b0),.sum(n5678[1:0]),.c_out(n
 // Sum neighbors using a 3-bit parallel adder
 adder_parallel_3 SUM_N(.a(n1234),.b(n5678),.c_in(1'b0),.sum(live_neighbors[2:0]),.c_out(live_neighbors[3]));
 
-logic next_state; 
 always_comb begin: INPUT_LOGIC
-    next_state = (~live_neighbors[3]&~live_neighbors[2]&live_neighbors[1])& ((state_q & ~live_neighbors[0])|live_neighbors[0]); 
-    state_d = ~rst ? (ena ? next_state : state_q) : state_0;
-    /*
-    live_neighbors_3 =(~live_neighbors[3]&~live_neighbors[2]&live_neighbors[1]);
-    continue_alive=state_q&~live_neighbors[0];
-    state_d = ~rst ? (ena ? (live_neighbors_3 | continue_alive): state_q) : state_0;
-    */ 
+    next_state = (~live_neighbors[3]&~live_neighbors[2]&live_neighbors[1])& ((state_q & ~live_neighbors[0])|live_neighbors[0]); //logic for conway game of life 
+    state_d = ~rst ? (ena ? next_state : state_q) : state_0; //state muxes 
 end
-/*
-always_comb begin
-    enabled_output=(ena)?state_d:state_q;
-    reset_output=(rst)?state_0:enabled_output;
-end
-*/
 
 always_ff @(posedge clk) begin: D_FLIP_FLOP
     state_q <= state_d;

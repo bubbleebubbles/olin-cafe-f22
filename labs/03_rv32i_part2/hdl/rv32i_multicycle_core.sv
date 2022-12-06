4d62886418226124939a9d728fd1fa83a615f491
Cara Mulrooney <cmulrooney@olin.edu>
2022-12-06 18:01:26 -0500

    Implement main fsm

--- labs/03_rv32i_part2/hdl/rv32i_multicycle_core.sv
+++ labs/03_rv32i_part2/hdl/rv32i_multicycle_core.sv
@@ -72,6 +72,9 @@ alu_behavioural ALU (
 // Implement your multicycle rv32i CPU here!
 
 // ALU Muxes
+enum logic [1:0] {ALU_SRC_A_PC, ALU_SRC_A_RF, ALU_SRC_A_OLD_PC} alu_src_a;
+enum logic [1:0] {ALU_SRC_B_RF, ALU_SRC_B_IMM, ALU_SRC_B_4} alu_src_b;
+
 always_comb begin: ALU_MUX_SRC_A
   case (alu_src_a)
     default: src_a=0;
@@ -94,10 +97,12 @@ end
 
 
 // Decoding instructions
+/*
 logic [6:0] op;
 logic r_type, i_type, l_type, s_type, b_type, j_type;
 enum logic [1:0] {IMM_SRC_I_TYPE, IMM_SRC_S_TYPE, IMM_SRC_B_TYPE, IMM_SRC_J_TYPE} immediate_src;
 logic [31:0] extended_immediate; 
+
 
 // Op type comparators
 r_type = (op==OP_R_TYPE);
@@ -115,9 +120,11 @@ case(op)
   OP_B_TYPE:  immediate_src = IMM_SRC_B_TYPE;
   OP_JAL, OP_JALR:  immediate_src = IMM_SRC_J_TYPE;
 
+*/
+
 // Main FSM
 
-typedef enum [3:0] {S_FETCH, S_DECODE, S_MEMADR, S_EXECUTER, S_EXECUTEI, S_JUMP, S_BRANCH, S_MEMREAD, S_ALUWB, S_MEMREAD, S_MEMWRITE, S_MEMWB, S_ERROR=4'hF } statetype;
+typedef enum [3:0] {S_FETCH, S_DECODE, S_MEMADR, S_EXECUTER, S_EXECUTEI, S_JUMP, S_BRANCH, S_MEMREAD, S_ALUWB, S_MEMREAD, S_MEMWRITE, S_MEMWB, S_BEQ, S_JAL, S_ERROR=4'hF } statetype;
 
 statetype state, next_state;
 logic [14:0] controls;
@@ -130,31 +137,45 @@ always @(posedge clk or posedge rst) begin : FSM_MULTICYCLE
 // Logic for next state
 always_comb: begin
   case(state)
-    default: next_state = S_ERROR;
+    default: next_state = S_FETCH;
     S_FETCH: next_state = S_DECODE;
     S_DECODE: begin
-      case(op)
+      casez(op) 
+      // casez is a "wildcard" case expression that allows "Z" and "?" to be treated 
+      // as don't care values in either the case expression and/or the case item when 
+      // doing a comparison
         default: begin
           $display("Error - op %b not implemented", op);
           next_state = S_ERROR;
         end
+        /*
         OP_R_TYPE: next_state = S_EXECUTER; // R-type
         OP_I_TYPE: next_state = S_EXECUTEI; // I-type ALU
         OP_S_TYPE, OP_L_TYPE, OP_LUI: next_state = S_MEMADR; // lw or sw
         OP_JAL: next_state = S_JUMP;
         OP_JALR: next_state = S_BRANCH;
+        */
+        7'b0?00011: next_state = S_MEMADR; // lw or sw
+        7'b0110011: next_state = S_EXECUTER; // R-type
+        7'b0010011: next_state = S_EXECUTEI; // I-type
+        7'b1101111: next_state = S_JAL; // jal
+        7'b1100011: next_state = S_BEQ; // beq
       endcase
     end
     S_MEMADR: begin
       case(op)
         default: next_state = S_ERROR;
+        /*
         OP_L_TYPE, OP_LUI: next_state = S_MEMREAD;
         OP_S_TYPE: next_state = S_MEMWRITE;
+        */
+        if (op[5]) next_state = S_MEMWRITE; // sw
+        else next_state = S_MEMREAD; // lw
       endcase
     end
-    S_EXECUTEI: next_state = S_ALUWB;
-    S_EXECUTER: next_state = S_ALUWB;
-    S_ALUWB, S_MEMREAD, S_MEMWRITE: next_state = S_FETCH
+    S_MEMREAD: next_state = S_MEMWB;
+    S_EXECUTEI, S_EXECUTER, S_JAL: next_state = S_ALUWB;
+    S_ALUWB, S_MEMREAD, S_MEMWRITE: next_state = S_FETCH;
     S_JUMP: begin
       $display("Please implement jumps")
       next_state = S_ERROR;
@@ -164,5 +185,49 @@ always_comb: begin
       next_state = S_ERROR;
     end
 end
+
+
+// state output logic
+always_comb: begin
+  case(state)
+    // AdrSrc_IRWrite_ALUSrcA_ALUSrcB_ALUOp_ResultSrc_PCUpdate_RegWrite_MemWrite_Branch
+    S_FETCH: controls=15'b0_1_00_10_00_10_1_0_0_0;
+    S_DECODE: controls=15'b0_0_01_01_00_00_0_0_0_0;
+    S_MEMADR: controls=15'b0_0_10_01_00_00_0_0_0_0;
+    S_MEMREAD: controls=15'b1_0_00_00_00_00_0_0_0_0;
+    S_MEMWRITE: controls=15'b1_0_00_00_00_00_0_0_1_0;
+    S_MEMWB: controls=15'b0_0_00_00_00_01_0_1_0_0;
+    S_EXECUTER: controls=15'b0_0_10_00_10_00_0_0_0_0;
+    S_EXECUTEI: controls=15'b0_0_10_01_10_00_0_0_0_0;
+    S_ALUWB: controls=15'b0_0_00_00_00_00_1_0_0_0;
+    S_JAL: controls=15'b0_0_10_00_01_00_0_0_0_1;
+    S_BEQ: controls=15'b0_0_01_10_00_00_1_0_0_0;
+    default: controls=15'bx_x_xx_xx_xx_xx_x_x_x_x;
+  endcase
+end
+
+assign {AdrSrc, IRWrite, ALUSrcA, ALUSrcB, ALUOp, ResultSrc, PCUpdate, RegWrite, MemWrite, Branch} = controls
+
+        // add
+        // sub
+        // xor
+        // or
+        // and
+        // sll
+        // srl
+        // sra
+        // slt
+        // sltu
+        // addi
+        // xori
+        // ori
+        // andi
+        // slli
+        // srli
+        // srai
+        // slti
+        // sltiu
+        // jalr
+        // bne
 
 endmodule
